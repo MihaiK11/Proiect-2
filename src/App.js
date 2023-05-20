8ff5d3fac15bebb5e6c7c95adb3ba34805b6a7a9
Mihai <mihail.crivoi00@e-uvt.ro>
2023-05-20 22:14:31 +0300

    Adaugare pagina servicii

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Yo</h1>
+        <div className="title">
+            <h1>Book your time</h1>
+        </div>
+        <div className="paginaServiciu">
+            <div className="slideShow">
+                <img src={require('./imagines/frizerie.jpg')} alt="Description of the image"></img>
+                <img src={require('./imagines/frizerie.jpg')} alt="Description of the image"></img>
+                <img src={require('./imagines/frizerie.jpg')} alt="Description of the image"></img>
+            </div>
+            <div className="info">
+                <a href="hz.com"><div className="companyName">Nume companiei:</div></a>
+                <div className="street">Strada:</div>
+                <div className="Contacts">Contacte:</div>
+                <div className="Price">Pret:</div>
+            </div>
+            <div className="depunereCerere">
+                <h2>Depunere</h2>
+            </div>
+        </div>
     </div>
   );
 }
