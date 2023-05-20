7900b982b701ad2a13d237eeedf6dd2940e72c84
Andrei Sviridov <Andrei_Sviridov@Andreis-MacBook-Pro.local>
2023-05-20 21:03:53 +0300

    prima pag

--- src/App.js
+++ src/App.js
@@ -6,47 +6,63 @@ function App() {
     <div className="App">
        <div className="title">
         <h1>Book your time</h1>
+        <br />
+        <br />
+        <br />
+        <br />
       </div>
+      <hr className="line" />
       <div className="scroll-layout">
         <h2>Frizerie</h2>
         <p>Frizerie: 9:00-20:00</p>
-        <div className="images-container">
+        <a href='exemplu.com'>
+          <div className="images-container">
           <img src={require('./images/image1.jpg')} alt="Imagine 1" />
           <img src={require('./images/image2.jpg')} alt="Imagine 2" />
           <img src={require('./images/image3.jpg')} alt="Imagine 3" />
-        </div>
+          </div>
+        </a>
+       
       </div>
-
+      <hr className="line" />
       <div className="scroll-layout">
         <h2>Salon de frumusete:</h2>
         <p>10:00-17:00</p>
-        <div className="images-container">
+        <a href='exemplu.com'>
+          <div className="images-container">
           <img src={require('./images/image4.jpg')} alt="Imagine 4" />
           <img src={require('./images/image5.jpg')} alt="Imagine 5" />
           <img src={require('./images/image6.jpg')} alt="Imagine 6" />
-        </div>
+          </div>
+        </a>
       </div>
-
+      <hr className="line" />
       <div className="scroll-layout">
         <h2>Carting : </h2>
         <p>9:00-19:00</p>
-        <div className="images-container">
+        <a href='exemplu.com'>
+          <div className="images-container">
           <img src={require('./images/image7.jpg')} alt="Imagine 7" />
           <img src={require('./images/image8.jpg')} alt="Imagine 8" />
           <img src={require('./images/image9.jpg')} alt="Imagine 9" />
-        </div>
+          </div>
+        </a>
       </div>
-
+      <hr className="line" />
       <div className="scroll-layout">
         <h2>Carwash</h2>
         <p>Carwash : 24/7</p>
-        <div className="images-container">
+        <a href='exemplu.com'>
+          <div className="images-container">
           <img src={require('./images/image10.jpg')} alt="Imagine 10" />
           <img src={require('./images/image11.jpg')} alt="Imagine 11" />
           <img src={require('./images/image12.jpg')} alt="Imagine 12" />
-        </div>
+          </div>
+        </a>
       </div>
+      <hr className="line" />
     </div>
+
   );
 }
 
